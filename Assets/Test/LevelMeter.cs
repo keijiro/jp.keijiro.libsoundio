640df27e1c44679e7af28979a33616c0ec6443ab
Keijiro Takahashi <keijiro@gmail.com>
2020-03-06 16:26:01 +0900

    Use single instance to control level meters

--- Assets/Test/LevelMeter.cs
+++ Assets/Test/LevelMeter.cs
@@ -8,12 +8,12 @@ public sealed class LevelMeter : MonoBehaviour
 {
     #region Editable attributes
 
-    enum FilterMode { Bypass, Lowpass, Bandpass, Highpass }
-
     [SerializeField] DeviceSelector _selector = null;
-    [SerializeField] FilterMode _filterMode = FilterMode.Bypass;
     [SerializeField] float _range = 60;
-    [SerializeField] RectTransform _meter = null;
+    [SerializeField] RectTransform _bypassMeter = null;
+    [SerializeField] RectTransform _lowPassMeter = null;
+    [SerializeField] RectTransform _bandPassMeter = null;
+    [SerializeField] RectTransform _highPassMeter = null;
 
     #endregion
 
@@ -39,7 +39,7 @@ public sealed class LevelMeter : MonoBehaviour
         }
 
         // Root mean square
-        var rms = math.sqrt(ss[(int)_filterMode] / _selector.AudioData.Length);
+        var rms = math.sqrt(ss / _selector.AudioData.Length);
 
         // RMS in dBFS
         // Full scale sin wave = 0 dBFS : refLevel = 1/sqrt(2)
@@ -49,7 +49,12 @@ public sealed class LevelMeter : MonoBehaviour
 
         // Meter scale
         var sc = math.max(0, _range + lv) / _range;
-        _meter.transform.localScale = new Vector3(sc, 1, 1);
+
+        // Apply to rect-transforms.
+          _bypassMeter.transform.localScale = new Vector3(sc.x, 1, 1);
+         _lowPassMeter.transform.localScale = new Vector3(sc.y, 1, 1);
+        _bandPassMeter.transform.localScale = new Vector3(sc.z, 1, 1);
+        _highPassMeter.transform.localScale = new Vector3(sc.w, 1, 1);
     }
 
     #endregion
