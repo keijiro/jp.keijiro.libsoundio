0e79593bb15f20e20fc4a97ec8fbe223f385c34b
Keijiro Takahashi <keijiro@gmail.com>
2020-03-06 19:59:18 +0900

    Jobified level meter

--- Assets/Test/LevelMeter.cs
+++ Assets/Test/LevelMeter.cs
@@ -1,8 +1,10 @@
 using SoundIO.SimpleDriver;
+using Unity.Collections;
+using Unity.Jobs;
 using Unity.Mathematics;
 using UnityEngine;
 
-// Level meter with a low/mid/high filter bank
+// Level meter with low/mid/high filter bank
 
 public sealed class LevelMeter : MonoBehaviour
 {
@@ -17,44 +19,81 @@ public sealed class LevelMeter : MonoBehaviour
 
     #endregion
 
-    #region Internal object
+    #region Filter + RMS level calculation job
 
-    MultibandFilter _filter;
+    [Unity.Burst.BurstCompile(CompileSynchronously = true)]
+    struct FilterRmsJob : IJob
+    {
+        [ReadOnly] public NativeArray<float> Input;
+        [WriteOnly] public NativeArray<float4> Output;
+        public NativeArray<MultibandFilter> Filter;
+
+        public void Execute()
+        {
+            var filter = Filter[0];
+
+            // Square sum
+            var ss = float4.zero;
+
+            for (var i = 0; i < Input.Length; i++)
+            {
+                var vf = filter.FeedSample(Input[i]);
+                ss += vf * vf;
+            }
+
+            // Root mean square
+            var rms = math.sqrt(ss / Input.Length);
+
+            // RMS in dBFS
+            // Full scale sin wave = 0 dBFS : refLevel = 1/sqrt(2)
+            const float refLevel = 0.7071f;
+            const float zeroOffset = 1.5849e-13f;
+            var level = 20 * math.log10(rms / refLevel + zeroOffset);
+
+            // Output
+            Output[0] = level;
+            Filter[0] = filter;
+        }
+    }
 
     #endregion
 
     #region MonoBehaviour implementation
 
+    MultibandFilter _filter;
+
     void Update()
     {
+        // Single element native array used to share structs with the job.
+        var tempFilter = new NativeArray<MultibandFilter>
+          (1, Allocator.TempJob, NativeArrayOptions.UninitializedMemory);
+
+        var tempLevel = new NativeArray<float4>
+          (1, Allocator.TempJob, NativeArrayOptions.UninitializedMemory);
+
+        // Filter update
         _filter.SetParameter(960.0f / _selector.SampleRate, 0.15f);
+        tempFilter[0] = _filter;
 
-        // Square sum
-        var ss = float4.zero;
+        // Run the job on the main thread.
+        new FilterRmsJob { Input = _selector.AudioData.GetNativeArray(),
+                           Filter = tempFilter, Output = tempLevel }.Run();
 
-        foreach (var v in _selector.AudioData)
-        {
-            var vf = _filter.FeedSample(v);
-            ss += vf * vf;
-        }
-
-        // Root mean square
-        var rms = math.sqrt(ss / _selector.AudioData.Length);
-
-        // RMS in dBFS
-        // Full scale sin wave = 0 dBFS : refLevel = 1/sqrt(2)
-        const float refLevel = 0.7071f;
-        const float zeroOffset = 1.5849e-13f;
-        var lv = 20 * math.log10(rms / refLevel + zeroOffset);
+        // Preserve the filter state.
+        _filter = tempFilter[0];
 
         // Meter scale
-        var sc = math.max(0, _range + lv) / _range;
+        var sc = math.max(0, _range + tempLevel[0]) / _range;
 
         // Apply to rect-transforms.
           _bypassMeter.transform.localScale = new Vector3(sc.x, 1, 1);
          _lowPassMeter.transform.localScale = new Vector3(sc.y, 1, 1);
         _bandPassMeter.transform.localScale = new Vector3(sc.z, 1, 1);
         _highPassMeter.transform.localScale = new Vector3(sc.w, 1, 1);
+
+        // Cleaning the temporaries up.
+        tempFilter.Dispose();
+        tempLevel.Dispose();
     }
 
     #endregion
--- Assets/Test/Utility.cs
+++ Assets/Test/Utility.cs
@@ -6,33 +6,39 @@ using Unity.Collections.LowLevel.Unsafe;
 
 static class SpanNativeArrayExtensions
 {
-    public static NativeArray<T>
-        GetNativeArray<T>(this ReadOnlySpan<T> span) where T : unmanaged
+    static AtomicSafetyHandle SpanSafetyHandle
+      = GetSpanSafetyHandle();
+
+    static AtomicSafetyHandle _spanSafetyHandle;
+
+    static AtomicSafetyHandle GetSpanSafetyHandle()
     {
-        unsafe
+        _spanSafetyHandle = AtomicSafetyHandle.Create();
+        return _spanSafetyHandle;
+    }
+
+    public unsafe static NativeArray<T>
+      GetNativeArray<T>(this ReadOnlySpan<T> span) where T : unmanaged
+    {
+        fixed (void* ptr = &span.GetPinnableReference())
         {
-            fixed (void* ptr = &span.GetPinnableReference())
-            {
-                var array = NativeArrayUnsafeUtility.
-                    ConvertExistingDataToNativeArray<T>
-                        (ptr, span.Length, Allocator.None);
-
-                #if ENABLE_UNITY_COLLECTIONS_CHECKS
-                var handle = AtomicSafetyHandle.GetTempUnsafePtrSliceHandle();
-                NativeArrayUnsafeUtility.SetAtomicSafetyHandle(ref array, handle);
-                #endif
-
-                return array;
-            }
+            var array = NativeArrayUnsafeUtility.
+              ConvertExistingDataToNativeArray<T>
+              (ptr, span.Length, Allocator.None);
+            #if ENABLE_UNITY_COLLECTIONS_CHECKS
+            NativeArrayUnsafeUtility.
+              SetAtomicSafetyHandle(ref array, SpanSafetyHandle);
+            #endif
+            return array;
         }
     }
 
-    public static ReadOnlySpan<T>
-        GetReadOnlySpan<T>(this NativeArray<T> array) where T : unmanaged
+    public unsafe static ReadOnlySpan<T>
+      GetReadOnlySpan<T>(this NativeArray<T> array) where T : unmanaged
     {
-        unsafe {
-            var ptr = NativeArrayUnsafeUtility.GetUnsafeReadOnlyPtr(array);
-            return new Span<T>(ptr, array.Length);
-        }
+        return new Span<T>(
+          NativeArrayUnsafeUtility.GetUnsafeReadOnlyPtr(array),
+          array.Length
+        );
     }
 }
